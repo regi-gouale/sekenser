7e457bec299739e5940d9096599ef068bd9b28c1
Régi GOUALE <regi.gouale@gmail.com>
2024-08-07 11:51:14 +0200

    init project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import { Inter, Orbitron } from "next/font/google";
+import "@/styles/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+const orbitron = Orbitron({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +16,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="fr">
+      <body className={orbitron.className}>{children}</body>
     </html>
   );
 }
